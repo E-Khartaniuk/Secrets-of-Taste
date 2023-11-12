fd716ce9236c67904c49e1d207fa7cf981d563c6
E-Khartaniuk <115401732+E-Khartaniuk@users.noreply.github.com>
2023-11-12 14:25:05 +0200

    fix

---
+++ src/components/Utils/FetchById.jsx
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+export default function FetchById(id) {
+  const URL = `https://api.spoonacular.com/recipes/${id}/information?apiKey=573176e8f1234f0eb2ab1bc5db75f54f`;
+  try {
+    const response = axios.get(URL);
+
+    return response;
+  } catch (error) {}
+}
--- src/components/pages/FavoriteDishes/FavoriteDishes.jsx
+++ src/components/pages/FavoriteDishes/FavoriteDishes.jsx
@@ -1,7 +1,42 @@
-import React from 'react';
+import FoodCard from 'components/FoodCard/FoodCard';
+import FetchById from 'components/Utils/FetchById';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function FavoriteDishes() {
-  return <div>FavoriteDishes</div>;
+  const navigate = useNavigate();
+  const [food, setFood] = useState([]);
+
+  useEffect(() => {
+    FetchById('665620,716406').then(res => {
+      console.log('reesssssssssssssssssssssssss', ree);
+      setFood(res.data.recipes);
+    });
+  }, []);
+
+  const handleOpenFullRecipe = recipeId => {
+    navigate(`/recipe/${recipeId}`);
+  };
+
+  return (
+    <section>
+      <ul
+      // className={css.dishesList}
+      >
+        {food &&
+          food.map(oneDish => {
+            return (
+              <FoodCard
+                key={oneDish.id}
+                cardInfo={oneDish}
+                // className={cardCSS.oneDishCard}
+                handleOpenFullRecipe={handleOpenFullRecipe}
+              ></FoodCard>
+            );
+          })}
+      </ul>
+    </section>
+  );
 }
 
 export default FavoriteDishes;
