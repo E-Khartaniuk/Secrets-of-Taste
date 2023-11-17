3cb1ab1b774c58a65d8058b15d5e17956bb076ef
E-Khartaniuk <115401732+E-Khartaniuk@users.noreply.github.com>
2023-11-18 00:57:17 +0200

    fix footer css

--- src/components/pages/FullRecipe/FullRecipe.jsx
+++ src/components/pages/FullRecipe/FullRecipe.jsx
@@ -16,61 +16,59 @@ function FullRecipe() {
   }, [id]);
 
   return (
-    <>
-      <div className={css.fullRecipeContainer}>
-        <h1 className={css.title}>{fullRecipe.title}</h1>
-        <div className={css.fullRecipeHeadContainer}>
-          <img
-            src={fullRecipe.image}
-            alt={fullRecipe.title}
-            className={css.recipeImg}
-          />
+    <div className={css.fullRecipeContainer}>
+      <h1 className={css.title}>{fullRecipe.title}</h1>
+      <div className={css.fullRecipeHeadContainer}>
+        <img
+          src={fullRecipe.image}
+          alt={fullRecipe.title}
+          className={css.recipeImg}
+        />
 
-          <div className={css.recipePropertyComtainer}>
-            <p className={css.recipeProperty}>
-              Cooking time: {fullRecipe.readyInMinutes} minutes
-            </p>
+        <div className={css.recipePropertyComtainer}>
+          <p className={css.recipeProperty}>
+            Cooking time: {fullRecipe.readyInMinutes} minutes
+          </p>
 
-            {fullRecipe.vegetarian ? (
-              <p className={css.recipeProperty}>Vegetarian</p>
-            ) : (
-              <p className={css.recipeProperty}>Vegetarian: no</p>
-            )}
+          {fullRecipe.vegetarian ? (
+            <p className={css.recipeProperty}>Vegetarian</p>
+          ) : (
+            <p className={css.recipeProperty}>Vegetarian: no</p>
+          )}
 
-            {fullRecipe.vegan ? (
-              <p className={css.recipeProperty}>Vegan</p>
-            ) : (
-              <p className={css.recipeProperty}>Vegan: no</p>
-            )}
+          {fullRecipe.vegan ? (
+            <p className={css.recipeProperty}>Vegan</p>
+          ) : (
+            <p className={css.recipeProperty}>Vegan: no</p>
+          )}
 
-            {fullRecipe.veryHealthy ? (
-              <p className={css.recipeProperty}>Healthy</p>
-            ) : (
-              <p className={css.recipeProperty}>Unhealthy</p>
-            )}
+          {fullRecipe.veryHealthy ? (
+            <p className={css.recipeProperty}>Healthy</p>
+          ) : (
+            <p className={css.recipeProperty}>Unhealthy</p>
+          )}
 
-            {fullRecipe.glutenFree ? (
-              <p className={css.recipeProperty}>Gluten free</p>
-            ) : (
-              <p className={css.recipeProperty}>Gluten free: no</p>
-            )}
-          </div>
+          {fullRecipe.glutenFree ? (
+            <p className={css.recipeProperty}>Gluten free</p>
+          ) : (
+            <p className={css.recipeProperty}>Gluten free: no</p>
+          )}
         </div>
-        <h3>Ingredients:</h3>
-
-        <ul className={css.listContainer}>
-          {fullRecipe.extendedIngredients &&
-            fullRecipe.extendedIngredients.map(ingridient => (
-              <li id={ingridient.id} className={css.ingridientListItem}>
-                <p>{ingridient.original}</p>
-              </li>
-            ))}
-        </ul>
-        {fullRecipe.instructions && <h3>How to cook it:</h3>}
-        <RecipeInstructions instructionsHTML={fullRecipe.instructions} />
-        <RecipeInstructions instructionsHTML={fullRecipe.summary} />
       </div>
-    </>
+      <h3>Ingredients:</h3>
+
+      <ul className={css.listContainer}>
+        {fullRecipe.extendedIngredients &&
+          fullRecipe.extendedIngredients.map(ingridient => (
+            <li id={ingridient.id} className={css.ingridientListItem}>
+              <p>{ingridient.original}</p>
+            </li>
+          ))}
+      </ul>
+      {fullRecipe.instructions && <h3>How to cook it:</h3>}
+      <RecipeInstructions instructionsHTML={fullRecipe.instructions} />
+      <RecipeInstructions instructionsHTML={fullRecipe.summary} />
+    </div>
   );
 }
 
