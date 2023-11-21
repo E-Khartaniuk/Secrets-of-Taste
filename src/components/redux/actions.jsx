3736acca755da6a2404a253e2bbf9bff38ae99ca
E-Khartaniuk <115401732+E-Khartaniuk@users.noreply.github.com>
2023-11-21 11:02:53 +0200

    Update actions.jsx

--- src/components/redux/actions.jsx
+++ src/components/redux/actions.jsx
@@ -1,15 +1,15 @@
-const state = useSelector(state => state.favorite);
+// const state = useSelector(state => state.favorite);
 
-export const togleFavorite = async cardinfo => {
-  if (state.favorite) {
-  } else {
-    setFavoriteIds(prevIds => [...prevIds, id]);
-  }
-  return addcontactToDB;
-};
+// export const togleFavorite = async cardinfo => {
+//   if (state.favorite) {
+//   } else {
+//     setFavoriteIds(prevIds => [...prevIds, id]);
+//   }
+//   return addcontactToDB;
+// };
 
-export const deleteFavorite = async id => {
-  const deleteContactFromDB = await instance.delete(`/contacts/${id}`);
+// export const deleteFavorite = async id => {
+//   const deleteContactFromDB = await instance.delete(`/contacts/${id}`);
 
-  return deleteContactFromDB;
-};
+//   return deleteContactFromDB;
+// };
